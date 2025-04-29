91e3cf92b957568169ea53235871505a3080ca6e
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-04-29 20:41:02 +0700

    init migration

---
+++ database/migrations/2025_03_05_132219_create_questionnaire_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Company;
+use App\Models\Profession;
+use App\Models\Superior;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questionnaires', function (Blueprint $table) {
+            $table->id();
+            $table->string('period_year', 4);
+            $table->enum('type', ['alumni', 'superior']);
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->boolean('is_active');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questionnaires');
+    }
+};
---
+++ database/migrations/2025_03_05_132221_create_answer_table copy.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Company;
+use App\Models\Profession;
+use App\Models\Question;
+use App\Models\Questionnaire;
+use App\Models\Superior;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->enum('filler_type', ['alumni', 'superior']);
+            $table->bigInteger('filler_id');
+            $table->foreignIdFor(Questionnaire::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Question::class)->constrained()->cascadeOnDelete();
+            $table->text('answer');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('answers');
+    }
+};
