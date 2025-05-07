53fa3dea9a4fbdeb9ed7305c51fac7809e31c463
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-05-07 09:05:04 +0700

    init tracer case alumni

---
+++ resources/views/landingPage/script/script-validate.blade.php
@@ -0,0 +1,60 @@
+<script>
+    var type = '{{ $questionnaire?->type }}';
+    var prodi = [];
+    $(()=> {
+        getOption();
+        $('#study_program').select2({
+            dropdownParent: $('.survey-cards-vertical')
+        });
+    })
+
+    getOption = () => {
+        $.ajax({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            url: `{{ route('fetch-option') }}`,
+            method: 'get',
+            success: (response) => {
+                prodi = response.prodi;
+                setOptionProdi()
+            }
+        })
+    }
+
+    setOptionProdi = () => {
+        $('#study_program').empty();
+        var html = `<option value="">-- Pilih Prodi --</option>`;
+        $.each(prodi, function(i, v) {
+            html += `<option value="${v}">${v}</option>`;
+        });
+        $('#study_program').append(html);
+    }
+
+
+    validateAlumni = () => {
+        var formData = new FormData($(`[name="form_alumni"]`)[0]);
+        loadBlock();
+        $.ajax({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            url: `{{ route('validate-alumni') }}`,
+            method: 'post',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: (res) => {
+                unblock();
+                saMessage({
+                    success: res['success'],
+                    title: res['title'],
+                    message: res['message'],
+                    callback: function() {
+                        window.location.href = "{{ route('questionnaire.content', $questionnaire->id) }}";
+                    }
+                })
+            }
+        })
+    }
+</script>
