22e29ce3ee2ef54f33fa4f1a2c67a46c8dc0ecc4
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-05-26 08:04:58 +0700

    fix some bug

--- resources/views/backoffice/questionnaire/javascriptAnswer.blade.php
+++ resources/views/backoffice/questionnaire/javascriptAnswer.blade.php
@@ -4,6 +4,19 @@
 
     $(() => {
         initTable();
+
+        $('#filter_study_program').select2({
+            dropdownParent: $('.filterModal')
+        });
+        $('#filter_company').select2({
+            dropdownParent: $('.filterModal')
+        });
+        $('#filter_profession_category').select2({
+            dropdownParent: $('.filterModal')
+        });
+        $('#filter_profession').select2({
+            dropdownParent: $('.filterModal')
+        });
     });
 
     const generateQuestionColumns = () => {
@@ -17,13 +30,12 @@
 
     const getDynamicColumns = () => {
         let columns = [{
-                data: null,
-                title: 'No',
-                orderable: false,
-                render: (data, type, row, meta) =>
-                    meta.row + meta.settings._iDisplayStart + 1
-            }
-        ];
+            data: null,
+            title: 'No',
+            orderable: false,
+            render: (data, type, row, meta) =>
+                meta.row + meta.settings._iDisplayStart + 1
+        }];
 
         if (type === 'alumni') {
             columns.push({
@@ -41,35 +53,28 @@
             }, {
                 data: 'email',
                 title: 'Email '
-            },
-            {
+            }, {
                 data: 'study_start_year',
                 title: 'Angkatan'
-            },
-            {
+            }, {
                 data: 'graduation_date',
                 title: 'Tanggal Lulus'
-            },
-            {
+            }, {
                 data: 'graduation_year',
                 title: 'Tahun Lulus'
-            },
-            {
+            }, {
                 data: 'start_work_date',
                 title: 'Tanggal Pertama Kerja'
-            },
-            {
+            }, {
                 data: 'waiting_time',
                 title: 'Masa Tunggu',
                 render: (data) => {
                     return `<span>${data ?? '-'} Bulan</span>`
                 }
-            },
-            {
+            }, {
                 data: 'start_work_now_date',
                 title: 'Tanggal Kerja Instansi'
-            },
-            {
+            }, {
                 data: 'company_type',
                 title: 'Jenis Instansi',
                 render: (data) => {
@@ -83,54 +88,44 @@
                         return `<span>Swasta</span>`;
                     }
                 }
-            },
-            {
+            }, {
                 data: 'company_name',
                 title: 'Nama Instansi'
-            },
-            {
+            }, {
                 data: 'company_scope',
                 title: 'Skala',
                 render: (data) => {
                     if (data === 'businessman') {
                         return `<span>Wirausaha</span>`;
-                    }else if (data === 'national') {
+                    } else if (data === 'national') {
                         return `<span>Nasional</span>`;
-                    }else if (data === 'international') {
+                    } else if (data === 'international') {
                         return `<span>Internasional</span>`;
                     }
 
                 }
-            },
-            {
+            }, {
                 data: 'company_address',
                 title: 'Lokasi'
-            },
-            {
+            }, {
                 data: 'profession_category',
                 title: 'Kategori Profesi'
-            },
-            {
+            }, {
                 data: 'profession',
                 title: 'Profesi'
-            },
-            {
+            }, {
                 data: 'superior_name',
                 title: 'Nama Atasan Langsung'
-            },
-            {
+            }, {
                 data: 'superior_position',
                 title: 'Jabatan Atasan Langsung'
-            },
-            {
+            }, {
                 data: 'superior_phone',
                 title: 'No. HP Atasan Langsung'
-            },
-            {
+            }, {
                 data: 'superior_email',
                 title: 'Email Atasan'
-            }
-        );
+            });
         } else if (type === 'superior') {
             columns.push({
                 data: 'full_name',
@@ -174,28 +169,26 @@
             }
         ];
 
- $('#table_answer').DataTable({
-  processing: true,
-  serverSide: true,
-  searching: true,
-  paging: true,
-  bDestroy: true,
-  ajax: {
-    url: "{{ route('backoffice.questionnaire.answer-table', ['id' => $data->id]) }}",
-    data: function(d) {
-      d.program_studi       = $('#program_studi').val();
-      d.nim                 = $('#filter_nim').val();
-      d.study_start_year    = $('#filter_study_start_year').val();
-      d.graduation_year     = $('#filter_graduation_year').val();
-      d.company             = $('#company').val();
-      d.scale               = $('#filter_scale').val();
-      d.profession_category = $('#profession_category').val();
-      d.profession          = $('#profession').val();
-      d.superior            = $('#superior').val();
-    }
-  },
-  columns: columns
-});
+        $('#table_answer').DataTable({
+            processing: true,
+            serverSide: true,
+            searching: true,
+            paging: true,
+            bDestroy: true,
+            ajax: {
+                url: "{{ route('backoffice.questionnaire.answer-table', ['id' => $data->id]) }}",
+                data: function(d) {
+                    d.study_program = $('#filter_study_program').val();
+                    d.nim = $('#filter_nim').val();
+                    d.study_start_year = $('#filter_study_start_year').val();
+                    d.graduation_year = $('#filter_graduation_year').val();
+                    d.company_id = $('#filter_company').val();
+                    d.profession_category_id = $('#filter_profession_category').val();
+                    d.profession_id = $('#filter_profession').val();
+                }
+            },
+            columns: columns
+        });
 
 
         unblock();
@@ -242,133 +235,115 @@
             }
         });
     }
-let company_data = [];
-let profession_data = [];
-let profession_category_data = [];
-let superior_data = [];
-let prodi_data = [];
 
-// Saat halaman siap
-$(() => {
-    onFetchOptionForm(); // Tambahkan ini untuk kuesioner
-    initTable();
-});
-$('#program_studi, #company, #filter_scale, #profession_category, #profession, #superior').select2({
-    dropdownParent: $('.filterModal')
-});
+    let company_data = [];
+    let profession_data = [];
+    let profession_category_data = [];
+    let superior_data = [];
+    let prodi_data = [];
 
-
-function onFetchOptionForm() {
-    $.ajax({
-        url: "{{ route('backoffice.alumni.fetch-option') }}",
-        method: 'GET',
-        success: function(data) {
-            company_data = data.company;
-            profession_data = data.profession;
-            profession_category_data = data.profession_category;
-            superior_data = data.superior;
-            prodi_data = data.prodi;
-
-            setOptionProdi();
-            setOptionSkala();
-            setOptionCompany();
-            setOptionProfessionCategory();
-        }
+    $(() => {
+        onFetchOptionForm();
+        initTable();
     });
-}
-function setOptionProdi() {
-    $('#program_studi').empty();
-    let html = `<option value="">-- Pilih Program Studi --</option>`;
-    $.each(prodi_data, (i, v) => {
-        html += `<option value="${v}">${v}</option>`;
-    });
-    $('#program_studi').append(html);
-}
-function setOptionSkala() {
-    let html = `
+
+    function onFetchOptionForm() {
+        $.ajax({
+            url: "{{ route('backoffice.alumni.fetch-option') }}",
+            method: 'GET',
+            success: function(data) {
+                company_data = data.company;
+                profession_data = data.profession;
+                profession_category_data = data.profession_category;
+                superior_data = data.superior;
+                prodi_data = data.prodi;
+
+                setOptionProdi();
+                setOptionSkala();
+                setOptionCompany();
+                setOptionProfessionCategory();
+            }
+        });
+    }
+
+    function setOptionProdi() {
+        $('#filter_study_program').empty();
+        let html = `<option value="">-- Pilih Program Studi --</option>`;
+        $.each(prodi_data, (i, v) => {
+            html += `<option value="${v}">${v}</option>`;
+        });
+        $('#filter_study_program').append(html);
+    }
+
+    function setOptionSkala() {
+        let html = `
         <option value="">-- Pilih Skala --</option>
         <option value="local">Lokal</option>
         <option value="national">Nasional</option>
         <option value="international">Internasional</option>
     `;
-    $('#filter_scale').html(html);
-}
-function setOptionCompany() {
-    $('#company').empty();
-    let html = `<option value="">-- Pilih Perusahaan --</option>`;
-    $.each(company_data, (i, v) => {
-        html += `<option value="${v.id}">${v.name}</option>`;
-    });
-    $('#company').append(html);
-}
-function setOptionProfessionCategory() {
-    $('#profession_category').empty();
-    let html = `<option value="">-- Pilih Kategori Profesi --</option>`;
-    $.each(profession_category_data, (i, v) => {
-        html += `<option value="${v.id}">${v.name}</option>`;
-    });
-    $('#profession_category').append(html);
-}
+        $('#filter_scale').html(html);
+    }
 
-$('#profession_category').on('change', function() {
-    const id = $(this).val();
-    $('#profession').empty();
-    let html = `<option value="">-- Pilih Profesi --</option>`;
-    $.each(profession_data, (i, v) => {
-        if (v.profession_category_id == id) {
+    function setOptionCompany() {
+        $('#filter_company').empty();
+        let html = `<option value="">-- Pilih Perusahaan --</option>`;
+        $.each(company_data, (i, v) => {
             html += `<option value="${v.id}">${v.name}</option>`;
-        }
-    });
-    $('#profession').append(html);
-});
-$('#company').on('change', function() {
-    const id = $(this).val();
-    $('#superior').empty();
-    let html = `<option value="">-- Pilih Atasan --</option>`;
-    $.each(superior_data, (i, v) => {
-        if (v.company_id == id) {
-            html += `<option value="${v.id}">${v.full_name}</option>`;
-        }
-    });
-    $('#superior').append(html);
-});
-function applyFilter() {
-    $('#filterModal').modal('hide');
+        });
+        $('#filter_company').append(html);
+    }
 
-    // Ambil ulang data table dengan parameter filter
-    $('#table_answer').DataTable().ajax.reload();
-}
-$(document).ready(function() {
-    // Mengubah tombol export agar bisa menyertakan filter
-    $('.btn-export-excel').on('click', function(e) {
-        e.preventDefault();
-        
-        // Ambil semua nilai filter yang sudah diinput
-        var program_studi = $('#filter_program_studi').val();
-        var nim = $('#filter_nim').val();
-        var study_start_year = $('#filter_study_start_year').val();
-        var graduation_year = $('#filter_graduation_year').val();
-        var company = $('#filter_company').val();
-        var scale = $('#filter_scale').val();
-        var profession_category = $('#filter_profession_category').val();
-        var profession = $('#filter_profession').val();
-        var superior = $('#filter_superior').val();
-        
-        // Buat URL dengan parameter filter
-        var url = "{{ route('backoffice.questionnaire.export-answer', $data->id) }}";
-        url += '?program_studi=' + (program_studi || '');
-        url += '&nim=' + (nim || '');
-        url += '&study_start_year=' + (study_start_year || '');
-        url += '&graduation_year=' + (graduation_year || '');
-        url += '&company=' + (company || '');
-        url += '&scale=' + (scale || '');
-        url += '&profession_category=' + (profession_category || '');
-        url += '&profession=' + (profession || '');
-        url += '&superior=' + (superior || '');
-        
-        // Arahkan browser ke URL export
-        window.location.href = url;
-    });
-});
+    function setOptionProfessionCategory() {
+        $('#filter_profession_category').empty();
+        let html = `<option value="">-- Pilih Kategori Profesi --</option>`;
+        $.each(profession_category_data, (i, v) => {
+            html += `<option value="${v.id}">${v.name}</option>`;
+        });
+        $('#filter_profession_category').append(html);
+    }
 
+    $('#filter_profession_category').on('change', function() {
+        const id = $(this).val();
+        $('#filter_profession').empty();
+        let html = `<option value="">-- Pilih Profesi --</option>`;
+        $.each(profession_data, (i, v) => {
+            if (v.profession_category_id == id) {
+                html += `<option value="${v.id}">${v.name}</option>`;
+            }
+        });
+        $('#filter_profession').append(html);
+    });
+
+    function applyFilter() {
+        $('#filterModal').modal('hide');
+
+        $('#table_answer').DataTable().ajax.reload();
+    }
+
+
+    $(document).ready(function() {
+        $('.btn-export-excel').on('click', function(e) {
+            e.preventDefault();
+
+            var study_program = $('#filter_study_program').val();
+            var nim = $('#filter_nim').val();
+            var study_start_year = $('#filter_study_start_year').val();
+            var graduation_year = $('#filter_graduation_year').val();
+            var company_id = $('#filter_company').val();
+            var profession_category_id = $('#filter_profession_category').val();
+            var profession_id = $('#filter_profession').val();
+
+            var url = "{{ route('backoffice.questionnaire.export-answer', $data->id) }}";
+            url += '?study_program=' + encodeURIComponent(study_program || '');
+            url += '&nim=' + encodeURIComponent(nim || '');
+            url += '&study_start_year=' + encodeURIComponent(study_start_year || '');
+            url += '&graduation_year=' + encodeURIComponent(graduation_year || '');
+            url += '&company_id=' + encodeURIComponent(company_id || '');
+            url += '&profession_category_id=' + encodeURIComponent(profession_category_id || '');
+            url += '&profession_id=' + encodeURIComponent(profession_id || '');
+
+            window.location.href = url;
+        });
+    });
 </script>
