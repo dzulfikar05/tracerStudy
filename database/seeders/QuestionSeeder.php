483f699a552076b89f1c10362fc36a2645743f6f
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-06-02 21:14:24 +0700

    fix seeder question

--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -22,54 +22,63 @@ class QuestionSeeder extends Seeder
                 'type' => 'essay',
                 'question' => 'Apa kesulitan yang anda alami pada saat masa pengenalan kerja',
                 'options' => null,
+                'is_assessment' => false
             ],
             [
                 'questionnaire_id' => 1,
                 'type' => 'choice',
                 'question' => 'Berapa Gaji anda',
                 'options' => json_encode(["<3 Juta", "3 juta sampai < 5 juta", "5 juta sampai < 10 juta", "> 10 juta"]),
+                'is_assessment' => false
             ],
             [
                 'questionnaire_id' => 2,
                 'type' => 'choice',
                 'question' => 'Kerjasama Tim',
                 'options' => json_encode(["Sangat Baik", "Baik", "Cukup", "Kurang"]),
+                'is_assessment' => true
             ],
             [
                 'questionnaire_id' => 2,
                 'type' => 'choice',
                 'question' => 'Keahlian di bidang IT',
                 'options' => json_encode(["Sangat Baik", "Baik", "Cukup", "Kurang"]),
+                'is_assessment' => true
             ],
             [
                 'questionnaire_id' => 2,
                 'type' => 'choice',
                 'question' => 'Kemampuan bahasa asing (Inggris)',
                 'options' => json_encode(["Sangat Baik", "Baik", "Cukup", "Kurang"]),
+                'is_assessment' => true
             ],
             [
                 'questionnaire_id' => 2,
                 'type' => 'choice',
                 'question' => 'Kemampuan berkomunikasi',
                 'options' => json_encode(["Sangat Baik", "Baik", "Cukup", "Kurang"]),
+                'is_assessment' => true
             ],
             [
                 'questionnaire_id' => 2,
                 'type' => 'choice',
                 'question' => 'Pengembangan diri',
                 'options' => json_encode(["Sangat Baik", "Baik", "Cukup", "Kurang"]),
+                'is_assessment' => true
             ],
             [
                 'questionnaire_id' => 2,
                 'type' => 'choice',
                 'question' => 'Kepemimpinan',
                 'options' => json_encode(["Sangat Baik", "Baik", "Cukup", "Kurang"]),
+                'is_assessment' => true
             ],
             [
                 'questionnaire_id' => 2,
                 'type' => 'choice',
                 'question' => 'Etos Kerja',
                 'options' => json_encode(["Sangat Baik", "Baik", "Cukup", "Kurang"]),
+                'is_assessment' => true
             ],
         ];
 
